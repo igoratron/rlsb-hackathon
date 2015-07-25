87eac69b0becd8112ffd3775bdcc94c9786d29cf
VD39 <you@example.com>
2015-07-25 15:11:38 +0100

    Updated to add route

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,40 @@
 angular
-  .module('RLSBApp', [])
-  .controller('RLSBController', function() {
-    console.log('Hello world');
-  });
+    .module('RLSBApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+
+                controller: 'HomeController',
+                template: '<p>HomeController</p>'
+
+            }).when('/job-search', {
+
+                controller: 'JobController',
+                template: '<p>JobController</p>'
+
+            }).when('/people', {
+
+                controller: 'PeopleController',
+                template: '<p>PeopleController</p>'
+
+            }).when('/tips', {
+
+                controller: 'TipsController',
+                template: '<p>TipsController</p>'
+
+            })
+
+
+    })
+    .controller('HomeController', function () {
+        console.log('Hello world');
+    })
+    .controller('PeopleController', function () {
+        console.log('Hello world');
+    })
+    .controller('JobController', function () {
+        console.log('Hello world');
+    })
+    .controller('TipsController', function () {
+        console.log('Hello world');
+    });
